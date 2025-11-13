f31f190def927376f8ebc6d15371cfb8709415b9
Akshat Pareek <62641746+AkshatPareek9@users.noreply.github.com>
2025-11-13 16:11:05 +0530

    Create ec2.tf

---
+++ code-dir/ec2.tf
@@ -0,0 +1,23 @@
+# instance name
+# image
+# flavor(type)
+# network
+# storage
+# count
+# key pair
+
+
+
+resource aws_ec2_instance my_instance {
+
+    key_name = 
+    security_groups = 
+    instance_type = 
+    ami = 
+    root_block_device = 
+    tags = {
+    
+    }
+
+}
+
