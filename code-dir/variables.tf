4f43c2ab084bfcf71ec43daaf7bd6577560ee98a
Akshat Pareek <62641746+AkshatPareek9@users.noreply.github.com>
2025-11-13 18:46:51 +0530

    Create variables.tf

---
+++ code-dir/variables.tf
@@ -0,0 +1,12 @@
+variable "ec2_instance_type" {
+  default="t2.micro"
+  type=string
+}
+variable "ec2_ami_id" {
+  default="ami-ag34bdfh6bh8324324fghdw4g5"
+  type=string
+}
+variable "ec2_storage_size" {
+  default="10"
+  type=number
+}
