04c167e8e2ed5ad4bc4e5459e5453d2d02fd4798
Akshat Pareek <62641746+AkshatPareek9@users.noreply.github.com>
2025-11-14 00:15:42 +0530

    Update outputs.tf for multi instances

--- code-dir/outputs.tf
+++ code-dir/outputs.tf
@@ -1,7 +1,9 @@
+# Add [*] if count of the instance is more than 1
+
 output "ec2_public_ip" {
-    value = aws_instance.my_instance.public_ip
+    value = aws_instance.my_instance[*].public_ip
 }
 
 output "ec2_private_ip" {
-    value = aws_instance.my_instance.private_ip
+    value = aws_instance.my_instance[*].private_ip
 }
