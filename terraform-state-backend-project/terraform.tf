b17a1d071ac85a170dd4662f903d9d25f04365e2
Akshat Pareek <62641746+AkshatPareek9@users.noreply.github.com>
2025-11-14 03:20:22 +0530

    Update terraform.tf

--- terraform-state-backend-project/terraform.tf
+++ terraform-state-backend-project/terraform.tf
@@ -5,4 +5,10 @@ terraform {
       version = "~> 6.0"
     }
   }
+  backend "s3" {
+    bucket = "terraform-state-bucket"
+    key = "terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "Terraform-State-Table"
+  }
 }
